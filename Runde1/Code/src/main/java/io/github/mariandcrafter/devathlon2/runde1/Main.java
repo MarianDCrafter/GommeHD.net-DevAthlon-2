a14cc288e241d0c02331046badd65393cef5c10d
Marian <MarianDietz@MacBook-Pro.fritz.box>
2015-07-09 15:24:50 +0200

    Initial commit

---
+++ Runde1/Code/src/main/java/io/github/mariandcrafter/devathlon2/runde1/Main.java
@@ -0,0 +1,13 @@
+package io.github.mariandcrafter.devathlon2.runde1;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+@SuppressWarnings("unused")
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("Hello world!");
+    }
+
+}
