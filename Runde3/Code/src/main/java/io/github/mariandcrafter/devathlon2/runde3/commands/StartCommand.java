bbb396d89a9ebdb9798d6304b2d83eb18de24b60
Marian <MarianDietz@MacBook-Pro-2.Speedport_W723_V_Typ_A_1_01_010>
2015-09-26 18:59:38 +0200

    two changes that were not contained in the submission, but are necessary for the small server we will publish

--- Runde3/Code/src/main/java/io/github/mariandcrafter/devathlon2/runde3/commands/StartCommand.java
+++ Runde3/Code/src/main/java/io/github/mariandcrafter/devathlon2/runde3/commands/StartCommand.java
@@ -1,12 +1,14 @@
 package io.github.mariandcrafter.devathlon2.runde3.commands;
 
 import io.github.mariandcrafter.bukkitpluginapi.commands.Command;
+import io.github.mariandcrafter.bukkitpluginapi.commands.CommandSenderType;
 import io.github.mariandcrafter.devathlon2.runde3.Main;
 
 public class StartCommand {
 
     @Command(
-            command = {"start"}
+            command = {"start"},
+            only = CommandSenderType.CONSOLE
     )
     public void start() {
         Main.getGameManager().startGame();
--- Runde3/Code/src/main/java/io/github/mariandcrafter/devathlon2/runde3/listeners/DoorListener.java
+++ Runde3/Code/src/main/java/io/github/mariandcrafter/devathlon2/runde3/listeners/DoorListener.java
@@ -2,6 +2,7 @@ package io.github.mariandcrafter.devathlon2.runde3.listeners;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class DoorListener implements Listener {
@@ -9,7 +10,7 @@ public class DoorListener implements Listener {
     @SuppressWarnings("unused")
     @EventHandler
     public void onInteract(PlayerInteractEvent event) {
-        if (event.getClickedBlock().getType().toString().contains("DOOR"))
+        if (event.getAction() == Action.RIGHT_CLICK_BLOCK && event.getClickedBlock().getType().toString().contains("DOOR"))
             event.setCancelled(true);
     }
 
